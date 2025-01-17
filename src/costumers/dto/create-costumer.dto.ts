d13de5ca89f5d8bf297e98075f3291bbb6d208e0
Daniele Quintiliani <quintiliani.d@gmail.com>
2025-01-17 22:56:13 +0100

    fixed costumers

--- src/costumers/dto/create-costumer.dto.ts
+++ src/costumers/dto/create-costumer.dto.ts
@@ -1,6 +1,5 @@
 export class CreateCostumerDto {
   name: string;
-  password: string;
   email: string;
   phone: string;
   address: string;
--- src/costumers/entities/costumer.entity.ts
+++ src/costumers/entities/costumer.entity.ts
@@ -3,7 +3,6 @@ import { Order } from '../../order/entities/order.entity';
 export class Costumers {
   id: number;
   name: string;
-  password: string;
   email: string;
   phone: string;
   address: string;
