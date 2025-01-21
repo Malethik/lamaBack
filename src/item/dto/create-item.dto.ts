d0edbe3b49df02472b3fcdba2a32fa0796b40917
Daniele Quintiliani <quintiliani.d@gmail.com>
2025-01-21 15:29:27 +0100

    fixed item create dto

--- src/item/dto/create-item.dto.ts
+++ src/item/dto/create-item.dto.ts
@@ -2,13 +2,13 @@ export class CreateItemDto {
   id: number; // Non è necessario, l'ID viene generato automaticamente
   name: string;
   price: number;
-  cost?: number = 0.0; // Opzionale, valore predefinito 0.0
+  cost?: number; // Opzionale, valore predefinito 0.0
   currency?: string = 'EURO'; // Opzionale, valore predefinito "EURO"
   category?: string = 'Uncategorized'; // Opzionale, valore predefinito "Uncategorized"
   tags?: string = 'Uncategorized'; // Opzionale, valore predefinito "Uncategorized"
   description: string;
   quantity: number;
-  lot?: string = '0'; // Opzionale, valore predefinito "0"
+  lot?: string; // Opzionale, valore predefinito "0"
   orderId?: number; // Opzionale poiché l'ordine è opzionale
   supplierId: number; // Deve essere fornito, singolo numero
 }
